cc50f4085672a35b6c8c22acd19756785c4bfb13
BainMcHale <158002431+BainMcHaleCMU@users.noreply.github.com>
2024-11-25 03:35:07 -0500

    GSM8K 8-shot pipeline

--- dataset/datasets.py
+++ dataset/datasets.py
@@ -1,27 +1,46 @@
 import pandas as pd
 import torch
-import transformers
+import numpy as np
 
 def read_jsonl(file_path):
     return pd.read_json(file_path, lines=True)
 
-class GSM8K_Dataset(torch.utils.data.Dataset):
-    def __init__(self, jsonl_file_path, tokenizer):
+class GSM8K_Train_Dataset(torch.utils.data.Dataset):
+    def __init__(self, jsonl_file_path, tokenizer, tiny=False):
         self.data = read_jsonl(jsonl_file_path)
-        self.data['question'] = self.data['question'].apply(tokenizer.tokenize)
-        self.data['answer'] = self.data['answer'].apply(tokenizer.tokenize)
-        longest_question = max(self.data['question'].apply(len))
-        longest_answer = max(self.data['answer'].apply(len))
-        self.max_len = longest_question + longest_answer
+        if tiny:
+          self.data = self.data[:100]
+        self.q_and_a = tokenizer((self.data['question'] + self.data['answer']).to_list(), padding='max_length', return_tensors='pt')
     
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, idx):
-        question = self.data['question'][idx]
-        answer = self.data['answer'][idx]
-        # add 0 padding
-        pad_size = self.max_len - len(question) - len(answer)
-        padding = [0] * pad_size
-        q_and_a = question + answer + padding
-        return (q_and_a, pad_size)
+        input_value = self.q_and_a['input_ids'][idx]
+        attention_mask = self.q_and_a['attention_mask'][idx]
+        return input_value, attention_mask
+
+class GSM8K_Val_Dataset(torch.utils.data.Dataset):
+    def __init__(self, jsonl_file_path, tokenizer, tiny=False):
+        self.data = pd.read_json(jsonl_file_path, lines=True)
+        if tiny:
+          self.data = self.data[:100]
+        shots1_8_ind = np.random.choice(self.data['question'].index, 8, replace=False)
+        shots1_8_q = self.data['question'][shots1_8_ind].to_list()
+        shots1_8_a = self.data['answer'][shots1_8_ind].to_list()
+        prompt_prefix = """// preamble or system instruction \
+                        As an expert problem solver, solve step by step the following mathematical questions.\n\n"""
+        questions = ["\n\n// shot-"+str(i)+"\nQ: " + shots1_8_q[i] + "\nA: Let's think step by step. " + shots1_8_a[i] + "\n" for i in range(8)]
+        prompt_prefix = "".join(questions)
+        prompt_prefix += "\n\n// target quesiton\nQ: "
+        prompt_suffix = "\nA: Let's think step by step. "
+        self.data['question'] = prompt_prefix + self.data['question'] + prompt_suffix
+        self.tok_question = tokenizer(self.data['question'].to_list(), padding=True, padding_side='left')
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        q = self.tok_question['input_ids'][idx]
+        q_mask = self.tok_question['attention_mask'][idx]
+        return q, q_mask, self.data['answer'][idx]
